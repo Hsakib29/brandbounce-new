b3e64efdd04dab2cc44a9c860cfa71e067f471c2
Mehdi Hasan Sakib <106773301+Hsakib29@users.noreply.github.com>
2025-09-16 02:21:10 +0600

    tiny css fix in Processcard

--- src/components/ProcessCard.tsx
+++ src/components/ProcessCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // Define the props for the ProcessCard component to ensure type safety.
 interface ProcessCardProps {
@@ -7,9 +7,13 @@ interface ProcessCardProps {
   icon?: React.ReactNode;
 }
 
-const ProcessCard: React.FC<ProcessCardProps> = ({ title, description, icon }) => {
+const ProcessCard: React.FC<ProcessCardProps> = ({
+  title,
+  description,
+  icon,
+}) => {
   return (
-    <div className="w-[297px] h-[297px] px-[15px] py-[13px] bg-[#F6F7F8] shadow-[0px_4px_4px_rgba(0,0,0,0.05)] overflow-hidden rounded-[20px] outline outline-1 outline-[rgba(255,255,255,0.60)] outline-offset-[-1px] flex flex-col justify-start items-start gap-[10px]">
+    <div className="w-[297px] h-[297px] px-[15px] py-[13px] bg-[#F6F7F8] shadow-[0px_4px_4px_rgba(0,0,0,0.05)] overflow-hidden rounded-[20px] outline outline-[rgba(255,255,255,0.60)] outline-offset-[-1px] flex flex-col justify-start items-start gap-[10px]">
       <div className="relative flex flex-col justify-start items-start gap-[10px]">
         <div className="w-[79px] h-[79px] bg-[#D9D9D9] shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)] rounded-[20px]" />
         {icon && (
